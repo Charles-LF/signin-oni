7e0a071085c93bc775b8a325bd767f08dae28a3d
Charles-ra <Charles@klei.vip>
2023-12-09 13:50:20 +0800

    删除无效config项目

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
 import { Context, Random, Schema, Time, h } from "koishi";
 import { sendMarkdown } from "./sendMarkdown";
 import * as db from "./db";
-import { Config } from "./config";
 
 export const name = "signin-oni";
 
 export const usage = `## 乱写(划掉)抄的签到,配合贼吉尔丑的图,将就着用`;
+
+export interface Config {
+  gold: number;
+  imgUrl: string;
+  yiyanapi: string;
+  eryan: string;
+  appId: string;
+  token: string;
+  templateId: string;
+  keyboardId: string;
+}
+
+export const Config: Schema<Config> = Schema.object({
+  gold: Schema.number().default(10).description("签到最多得到多少金币"),
+  imgUrl: Schema.string()
+    .default("https://kleiforums.s3.amazonaws.com/gameimages/457140-image.jpg")
+    .description("签到时使用的顶部图片地址"),
+  yiyanapi: Schema.string()
+    .default("http://api.guaqb.cn/v1/onesaid")
+    .description("一言请求的api"),
+  eryan: Schema.string()
+    .default("知识才应是一个人类最虔诚的信仰 (")
+    .description("签到时第二行显示的文字"),
+  appId: Schema.string().description("机器人ID"),
+  token: Schema.string().description("机器人token"),
+  templateId: Schema.string().description("模板ID"),
+  keyboardId: Schema.string().description("按钮ID"),
+});
 
 // 写得跟shit一样
 
